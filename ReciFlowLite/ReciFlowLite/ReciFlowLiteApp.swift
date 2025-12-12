506a15063f7416e7b73031d987412c903319ea32
Kishima Ikuo <kisi-man@users.noreply.github.com>
2025-12-12 11:22:32 +0900

    Initial ReciFlowLite project

---
+++ ReciFlowLite/ReciFlowLite/ReciFlowLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciFlowLiteApp.swift
+//  ReciFlowLite
+//
+//  Created by 木嶋育朗 on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ReciFlowLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReciFlowLite/ReciFlowLiteTests/ReciFlowLiteTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReciFlowLiteTests.swift
+//  ReciFlowLiteTests
+//
+//  Created by 木嶋育朗 on 2025/12/12.
+//
+
+import Testing
+
+struct ReciFlowLiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
